580afb847016f73e7dc72c542467f4bfc87f585a
Ulises-Lagunez <gwlises369@gmail.com>
2025-11-19 09:24:12 -0600

    Estructura del proyecto
    Archivos para el proyecto carrito dorado

---
+++ app/src/main/java/com/example/carritodorado/ui/pantallas/PantallaLogin.kt
@@ -0,0 +1,10 @@
+package com.example.carritodorado.ui.pantallas
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+
+fun PantallaLogin(navController: NavController){
+    
+}
